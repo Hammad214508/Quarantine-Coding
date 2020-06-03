cd821c5997f4a9584879d783f32da04222284a01
Hammad <hammadmuhammad15@gmail.com>
2020-06-03 09:57:15 +0100

    Two City Scheduling

--- 30-Day-LeetCoding-Challenge/June/README.md
+++ 30-Day-LeetCoding-Challenge/June/README.md
@@ -5,3 +5,5 @@
 Day 1 - Invert Binary Tree
 
 Day 2 - Delete Node in a Linked List
+
+Day 3 - Two City Scheduling
---
+++ 30-Day-LeetCoding-Challenge/June/Week1/3-TwoCityScheduling.py
@@ -0,0 +1,43 @@
+"""
+There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0],
+and the cost of flying the i-th person to city B is costs[i][1].
+
+Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
+
+Example 1:
+Input: [[10,20],[30,200],[400,50],[30,20]]
+Output: 110
+Explanation:
+    The first person goes to city A for a cost of 10.
+    The second person goes to city A for a cost of 30.
+    The third person goes to city B for a cost of 50.
+    The fourth person goes to city B for a cost of 20.
+
+The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+
+Note:
+    1 <= costs.length <= 100
+    It is guaranteed that costs.length is even.
+    1 <= costs[i][0], costs[i][1] <= 1000
+"""
+# We put all of them to 1 city and pay 10 + 30 + 400 + 30
+# Now, evaluate differences: 10, 170, -350, -10.
+# Choose 2 smallest differences, in this case it is -350 and -10 (they are negative, so we can say we get a refund)
+# Evaluate final sum as 10 + 30 + 400 + 300 + -350 + -10 = 110
+class Solution:
+    def twoCitySchedCost(self, costs: [[int]]) -> int:
+        costs.sort(key = lambda x: x[0] - x[1])
+        lth = len(costs) // 2
+        cost = 0
+        for i in range(lth):
+            cost += costs[i][0]
+        for i in range(lth, 2 * lth):
+            cost += costs[i][1]
+        return cost
+
+
+
+solution = Solution()
+list = [[10,20],[30,200],[400,50],[30,20]]
+res = solution.twoCitySchedCost(list)
+print(res)
