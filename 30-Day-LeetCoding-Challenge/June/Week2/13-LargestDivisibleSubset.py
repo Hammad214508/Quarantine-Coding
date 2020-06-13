b4f47fe75e072eaf127f7362c1af9f83026c980b
Hammad <hammadmuhammad15@gmail.com>
2020-06-13 11:47:38 +0100

    fixed largest divisible subset

--- 30-Day-LeetCoding-Challenge/June/Week2/13-LargestDivisibleSubset.py
+++ 30-Day-LeetCoding-Challenge/June/Week2/13-LargestDivisibleSubset.py
@@ -16,14 +16,14 @@ Example 2:
 """
 class Solution:
     def largestDivisibleSubset(self, nums: [int]) -> [int]:
-        if len(nums) == 0: return []
+        if len(nums) == 0:
+            return []
         nums.sort()
         sol = [[num] for num in nums]
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] % nums[j] == 0 and len(sol[i]) < len(sol[j]) + 1:
                     sol[i] = sol[j] + [nums[i]]
-        print(sol)
         return max(sol, key=len)
 
 
