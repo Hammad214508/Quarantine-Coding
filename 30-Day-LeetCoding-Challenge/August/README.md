cdef57e22d98be3ef03231d4f397f1b5fffb5675
Hammad214508 <hammadmuhammad15@gmail.com>
2020-08-03 21:54:07 +0100

    valid palindrome

--- 30-Day-LeetCoding-Challenge/August/README.md
+++ 30-Day-LeetCoding-Challenge/August/README.md
@@ -5,3 +5,5 @@
 Day 1 - Detect Capital
 
 Day 2 - Design HashSet
+
+Day 3 - Valid Palindrome
---
+++ 30-Day-LeetCoding-Challenge/August/Week1/3-ValidPalindrome.py
@@ -0,0 +1,23 @@
+"""
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+
+Constraints:
+
+s consists only of printable ASCII characters.
+"""
+class Solution:        
+    def isPalindrome(self, s):
+        s = [ x.lower() for x in s if x.isalnum() ]
+        return s == s[::-1]
